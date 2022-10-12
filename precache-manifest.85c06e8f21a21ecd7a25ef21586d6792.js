d36113593e7810d1645913b58ea979fca91ed141
laljidp <laljidp@users.noreply.github.com>
2022-10-12 07:10:44 +0000

    deploy: 8716c950eaabfc70c683e80017e192df1a96d127

---
+++ precache-manifest.85c06e8f21a21ecd7a25ef21586d6792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816af7eebedce2ed3de3c19931e8edd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f81faa2cc0201c9b8034",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "89ab56017ee42e6a0d3b",
+    "url": "/static/js/2.f7a00e9e.chunk.js"
+  },
+  {
+    "revision": "f81faa2cc0201c9b8034",
+    "url": "/static/js/main.bdf72ebe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
