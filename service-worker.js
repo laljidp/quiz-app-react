b390283b8f8d4bd419fa4cb6d4c5cd1452c648a5
laljidp <laljidp@users.noreply.github.com>
2022-10-12 07:23:55 +0000

    deploy: 949ecea7aec24443a463b7ff43455f2788aa0225

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85c06e8f21a21ecd7a25ef21586d6792.js"
+  "/quiz-app/precache-manifest.6a9dc140887ac0638f78ba13171ee8e5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/quiz-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
